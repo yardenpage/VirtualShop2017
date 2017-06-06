ad16c8ed33fb99f553d818c2fae8e3735e774c77
yardenpage <pagey@post.bgu.ac.il>
2017-06-06 17:22:13 +0300

    testtt2

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,7 @@ var user_id;
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
     res.send('respond with a resource');
+    res.send('respond with a resource');
 });
 
 router.post('/Login',function (req,res) {
