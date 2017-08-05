68045a6ade3c71d1e8ae11f498bd1a450f3558e9
yardenpage <pagey@post.bgu.ac.il>
2017-08-05 18:02:03 +0300

    xml countries

--- public/app.js
+++ public/app.js
@@ -168,6 +168,30 @@ app.controller('registerController', ['openPageService', '$location', '$window',
     function(openPageService, $location, $window, $http) {
         let self = this;
         self.user = {userName: '', password: '', firstName: '', lastName: '', gender: '', email: '', phone: '', address: '', country: '', securityAnswer: '', shirts: '', pants: '', dresses: '', skirts: '', underwears: ''};
+        self.loadXMLDoc= function() {
+            var xmlhttp = new XMLHttpRequest();
+            xmlhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    myFunction(this);
+                }
+            };
+            xmlhttp.open("GET", "countries.xml", true);
+            xmlhttp.send();
+        }
+
+        function myFunction(xml) {
+            var i;
+            var xmlDoc = xml.responseXML;
+            var temp = [];
+            var x = xmlDoc.getElementsByTagName("Country");
+            for (i = 0; i <x.length; i++) {
+                var json = { "ID" :x[i].getElementsByTagName("ID")[0].childNodes[0].nodeValue.toString(),
+                    "Name" :x[i].getElementsByTagName("Name")[0].childNodes[0].nodeValue.toString()}
+                temp.push(json);
+            }
+            self.Countries = temp;
+            self.user.country = self.Countries[4];
+        }
 
         self.register = function() {
             if(self.user.shirts=="")
